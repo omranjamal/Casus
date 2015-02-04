d1ff41d0620c309e9d3adece3297ec163fd23916
Omran Jamal <o.jamal97@gmail.com>
2015-02-04 15:07:55 +0600

    Started Constructor
    - Added a few Check methods
    - Tested the checker methods
    - Created Abstraction methods

--- Tests/CasusTest.php
+++ Tests/CasusTest.php
@@ -3,5 +3,21 @@ include_once '../vendor/autoload.php';
 
 class CasusTest extends PHPUnit_Framework_TestCase
 {
+	public function testHasMCrypt()
+	{
+		$casus = new \solidew\Casus\Casus();
+		$this->assertEquals(
+			function_exists("mcrypt_encrypt"),
+			$casus->hasMcrypt()
+		);
+	}
 
+	public function testHasOpenSSL()
+	{
+		$casus = new \solidew\Casus\Casus();
+		$this->assertEquals(
+			function_exists("openssl_random_pseudo_bytes"), 
+			$casus->hasOpenSSL()
+		);
+	}
 }
--- src/Casus.php
+++ src/Casus.php
@@ -1,6 +1,49 @@
 <?php
 namespace solidew\Casus;
 
-class Casus {
+class Casus 
+{
+	protected $generator;
+	protected $secure;
 
+	public function hasMCrypt()
+	{
+		return function_exists("mcrypt_encrypt");
+	}
+
+	public function hasOpenSSL()
+	{
+		return function_exists("openssl_random_pseudo_bytes");
+	}
+
+	public function isSecure()
+	{
+		return $this->secure;
+	}
+
+	public function getDenerator()
+	{
+		return $this->generator;
+	}
+
+	public function __construct($secure = true, Generator $generator = null)
+	{
+		if ($secure === true && $generator === null) {
+			if ($this->hasMCrypt()) {
+				$this->generator = new generators\MCrypt;
+			} elseif ($this->hasOpenSSL()) {
+				$this->generator = new generators\OpenSSL;
+			} else {
+				throw new errors\Insecure('MCrypt or OpenSSL extension not found');
+			}
+		} elseif ($generator) {
+			$this->generator = $generator;
+
+			if ($secure === true) {
+				if ($generator->secure === false) {
+					throw new error\Insecure('The provided generator is not a CSPRNG');
+				}
+			}
+		}
+	}
 }
