7c5212c5669d3097f28cfd4ee0420863c2aebba4
Omran Jamal <o.jamal97@gmail.com>
2015-02-08 17:49:56 +0600

    Written and Tested MCrypt Generator

--- src/MCrypt.php
+++ src/MCrypt.php
@@ -1,12 +1,23 @@
 <?php
 namespace solidew\Casus;
 
-class MCrypt extends \solidew\Casus\Generator 
+class MCrypt extends Generator 
 {
     public $secure = true;
 	
-	public function integer($mix, $max)
+	public function integer($min = 0, $max = PHP_INT_MAX)
 	{
-		
+		$bytes = mcrypt_create_iv(PHP_INT_SIZE, MCRYPT_DEV_URANDOM);
+		$num = 0;
+
+		for ($i = 0; $i < PHP_INT_SIZE; $i++) {
+			$num = ($num<<8)|ord($bytes[$i]);
+		}
+
+		if ($min>=0 && $max>=0) {
+			$num = abs($num);
+		}
+
+		return $min+($num%($max-$min+1));
 	}
 }
