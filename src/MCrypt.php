258a6e8ad781c6360ea859ca895f59fa345f97be
Omran Jamal <o.jamal97@gmail.com>
2015-02-08 23:22:58 +0600

    Added MCrypt Specific Byte generators

--- src/MCrypt.php
+++ src/MCrypt.php
@@ -4,10 +4,11 @@ namespace solidew\Casus;
 class MCrypt extends Generator 
 {
     protected $secure = true;
+    protected $source = MCRYPT_DEV_URANDOM;
 	
 	public function integer($min = 0, $max = PHP_INT_MAX)
 	{
-		$bytes = mcrypt_create_iv(PHP_INT_SIZE, MCRYPT_DEV_URANDOM);
+		$bytes = mcrypt_create_iv(PHP_INT_SIZE, $this->source);
 		$num = 0;
 
 		for ($i = 0; $i < PHP_INT_SIZE; $i++) {
@@ -20,4 +21,14 @@ class MCrypt extends Generator
 
 		return $min+($num%($max-$min+1));
 	}
+
+	public function byte($secure = true)
+    {
+        return mcrypt_create_iv(1, $this->source);
+    }
+
+    public function byteString($length = 32, $secure = true)
+    {
+        return mcrypt_create_iv($length, $this->source);
+    }
 }
