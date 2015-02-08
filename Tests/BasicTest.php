90b89bc7ca66c1dacf21e2f8cacccc2a6ea888b9
Omran Jamal <o.jamal97@gmail.com>
2015-02-08 22:24:38 +0600

    tested Randomize and Select Random

--- Tests/BasicTest.php
+++ Tests/BasicTest.php
@@ -34,6 +34,20 @@ namespace {
             $this->assertInternalType('float', $val);
         }
 
+        public function testFloatWithNegativePrecision()
+        {
+            $min = 3;
+            $max = 9;
+            $points = -3;
+
+            $casus = new \solidew\Casus\Basic;
+            $val = $casus->float($min, $max, $points);
+
+            $this->assertTrue($val<=$max && $val>=$min);
+            $this->assertRegExp('/^[0-9](\.[0-9]{0,3})?$/', (string)$val);
+            $this->assertInternalType('float', $val);
+        }
+
         public function testBoolean()
         {
             $casus = new \solidew\Casus\Basic;
@@ -62,6 +76,16 @@ namespace {
             $this->assertInternalType('string', $val);
         }
 
+        public function testAlphaWithNegativeLength()
+        {
+            $casus = new \solidew\Casus\Basic;
+            $val = $casus->alpha(-200, true);
+
+            $this->assertEquals(strlen($val), 200);
+            $this->assertRegExp('/^[a-zA-Z]+$/', (string)$val);
+            $this->assertInternalType('string', $val);
+        }
+
         public function testAlphaNumWithNoCaseRandomization()
         {
             $casus = new \solidew\Casus\Basic;
@@ -82,6 +106,16 @@ namespace {
             $this->assertInternalType('string', $val);
         }
 
+        public function testAlphaNumWithNegativeLength()
+        {
+            $casus = new \solidew\Casus\Basic;
+            $val = $casus->alphanum(-200, true);
+
+            $this->assertEquals(strlen($val), 200);
+            $this->assertRegExp('/^[a-zA-Z0-9]+$/', (string)$val);
+            $this->assertInternalType('string', $val);
+        }
+
         public function testAsciiRangeNested()
         {
             $casus = new \solidew\Casus\Basic;
@@ -91,8 +125,18 @@ namespace {
             $this->assertRegExp('/^a+$/', $val);
             $this->assertInternalType('string', $val);
 
-            $val = $casus->asciiRange(200, [[97,98]]);
-            $this->assertRegExp('/^[ab]+$/', $val);
+            $val = $casus->asciiRange(200, [[97,98],[99,100]]);
+            $this->assertRegExp('/^[abcd]+$/', $val);
+            $this->assertInternalType('string', $val);
+        }
+
+        public function testAsciiRangeNestedWithNegativeLength()
+        {
+            $casus = new \solidew\Casus\Basic;
+            $val = $casus->asciiRange(-200, [[97,97]]);
+
+            $this->assertEquals(strlen($val), 200);
+            $this->assertRegExp('/^a+$/', $val);
             $this->assertInternalType('string', $val);
         }
 
@@ -144,10 +188,29 @@ namespace {
             $this->assertInternalType('string', $val);
         }
 
+        public function testStringWithNegativeLength()
+        {
+            $casus = new \solidew\Casus\Basic;
+            $val = $casus->string(-40, $charset = 'a');
+
+            $this->assertEquals(strlen($val), 40);
+            $this->assertRegExp('/^a+$/', $val);
+            $this->assertInternalType('string', $val);
+        }
+
         public function testIntegerArray()
         {
             $casus = new \solidew\Casus\Basic;
             $val = $casus->integerArray(1,2,100);
+            $this->assertInternalType('array', $val);
+            $this->assertEquals(count($val), 100);
+            $this->assertContainsOnly('int', $val);
+        }
+
+        public function testIntegerArrayWithNegativeLength()
+        {
+            $casus = new \solidew\Casus\Basic;
+            $val = $casus->integerArray(1,2,-100);
             $this->assertInternalType('array', $val);
             $this->assertEquals(count($val), 100);
             $this->assertContainsOnly('int', $val);
@@ -160,6 +223,75 @@ namespace {
             $this->assertInternalType('array', $val);
             $this->assertEquals(count($val), 100);
             $this->assertContainsOnly('float', $val);
+        }
+
+        public function testFloatArrayWithNegativeLength()
+        {
+            $casus = new \solidew\Casus\Basic;
+            $val = $casus->floatArray(1,2,4,-100);
+            $this->assertInternalType('array', $val);
+            $this->assertEquals(count($val), 100);
+            $this->assertContainsOnly('float', $val);
+        }
+
+        public function testRandomizeWithString()
+        {
+            $casus = new \solidew\Casus\Basic;
+            $val = $casus->randomize('Potato');
+
+            $this->assertInternalType('string', $val);
+            $this->assertEquals(strlen($val), 6);
+            $this->assertTrue(in_array('P',str_split($val)));
+        }
+
+        public function testRandomizeWithArray()
+        {
+            $casus = new \solidew\Casus\Basic;
+            $val = $casus->randomize(['P','o','t','a','t','o']);
+
+            $this->assertInternalType('array', $val);
+            $this->assertEquals(count($val), 6);
+            $this->assertTrue(in_array('P', $val));
+        }
+
+        public function selectRandomWithString()
+        {
+            $casus = new \solidew\Casus\Basic;
+            $val = $casus->selectRandom('Potato', 3);
+
+            $this->assertInternalType('string', $val);
+            $this->assertEquals(strlen($val), 3);
+            $this->assertTrue(in_array(['P','o','t','a'],str_split($val)));
+        }
+
+        public function selectRandomWithArray()
+        {
+            $casus = new \solidew\Casus\Basic;
+            $val = $casus->selectRandom(['P','o','t','a','t','o'], 3);
+
+            $this->assertInternalType('array', $val);
+            $this->assertEquals(count($val), 3);
+            $this->assertTrue(in_array(['P','o','t','a'], $val));
+        }
+
+        public function selectRandomWithLengthBiggerThanInput()
+        {
+            $casus = new \solidew\Casus\Basic;
+            $val = $casus->selectRandom(['P','o','t','a','t','o'], 20);
+
+            $this->assertInternalType('array', $val);
+            $this->assertEquals(count($val), 6);
+            $this->assertTrue(in_array(['P','o','t','a'], $val));
+        }
+
+        public function selectRandomWithNegativeLength()
+        {
+            $casus = new \solidew\Casus\Basic;
+            $val = $casus->selectRandom(['P','o','t','a','t','o'], -20);
+
+            $this->assertInternalType('array', $val);
+            $this->assertEquals(count($val), 6);
+            $this->assertTrue(in_array(['P','o','t','a'], $val));
         }
     }
 }
--- Tests/MCryptTest.php
+++ Tests/MCryptTest.php
@@ -34,6 +34,20 @@ namespace {
             $this->assertInternalType('float', $val);
         }
 
+        public function testFloatWithNegativePrecision()
+        {
+            $min = 3;
+            $max = 9;
+            $points = -3;
+
+            $casus = new \solidew\Casus\MCrypt;
+            $val = $casus->float($min, $max, $points);
+
+            $this->assertTrue($val<=$max && $val>=$min);
+            $this->assertRegExp('/^[0-9](\.[0-9]{0,3})?$/', (string)$val);
+            $this->assertInternalType('float', $val);
+        }
+
         public function testBoolean()
         {
             $casus = new \solidew\Casus\MCrypt;
@@ -55,9 +69,19 @@ namespace {
         public function testAlphaWithCaseRandomization()
         {
             $casus = new \solidew\Casus\MCrypt;
-            $val = $casus->alpha(50, true);
+            $val = $casus->alpha(200, true);
 
-            $this->assertEquals(strlen($val), 50);
+            $this->assertEquals(strlen($val), 200);
+            $this->assertRegExp('/^[a-zA-Z]+$/', (string)$val);
+            $this->assertInternalType('string', $val);
+        }
+
+        public function testAlphaWithNegativeLength()
+        {
+            $casus = new \solidew\Casus\MCrypt;
+            $val = $casus->alpha(-200, true);
+
+            $this->assertEquals(strlen($val), 200);
             $this->assertRegExp('/^[a-zA-Z]+$/', (string)$val);
             $this->assertInternalType('string', $val);
         }
@@ -82,6 +106,16 @@ namespace {
             $this->assertInternalType('string', $val);
         }
 
+        public function testAlphaNumWithNegativeLength()
+        {
+            $casus = new \solidew\Casus\MCrypt;
+            $val = $casus->alphanum(-200, true);
+
+            $this->assertEquals(strlen($val), 200);
+            $this->assertRegExp('/^[a-zA-Z0-9]+$/', (string)$val);
+            $this->assertInternalType('string', $val);
+        }
+
         public function testAsciiRangeNested()
         {
             $casus = new \solidew\Casus\MCrypt;
@@ -91,8 +125,18 @@ namespace {
             $this->assertRegExp('/^a+$/', $val);
             $this->assertInternalType('string', $val);
 
-            $val = $casus->asciiRange(200, [[97,98]]);
-            $this->assertRegExp('/^[ab]+$/', $val);
+            $val = $casus->asciiRange(200, [[97,98],[99,100]]);
+            $this->assertRegExp('/^[abcd]+$/', $val);
+            $this->assertInternalType('string', $val);
+        }
+
+        public function testAsciiRangeNestedWithNegativeLength()
+        {
+            $casus = new \solidew\Casus\MCrypt;
+            $val = $casus->asciiRange(-200, [[97,97]]);
+
+            $this->assertEquals(strlen($val), 200);
+            $this->assertRegExp('/^a+$/', $val);
             $this->assertInternalType('string', $val);
         }
 
@@ -144,10 +188,29 @@ namespace {
             $this->assertInternalType('string', $val);
         }
 
+        public function testStringWithNegativeLength()
+        {
+            $casus = new \solidew\Casus\MCrypt;
+            $val = $casus->string(-40, $charset = 'a');
+
+            $this->assertEquals(strlen($val), 40);
+            $this->assertRegExp('/^a+$/', $val);
+            $this->assertInternalType('string', $val);
+        }
+
         public function testIntegerArray()
         {
             $casus = new \solidew\Casus\MCrypt;
             $val = $casus->integerArray(1,2,100);
+            $this->assertInternalType('array', $val);
+            $this->assertEquals(count($val), 100);
+            $this->assertContainsOnly('int', $val);
+        }
+
+        public function testIntegerArrayWithNegativeLength()
+        {
+            $casus = new \solidew\Casus\MCrypt;
+            $val = $casus->integerArray(1,2,-100);
             $this->assertInternalType('array', $val);
             $this->assertEquals(count($val), 100);
             $this->assertContainsOnly('int', $val);
@@ -160,6 +223,75 @@ namespace {
             $this->assertInternalType('array', $val);
             $this->assertEquals(count($val), 100);
             $this->assertContainsOnly('float', $val);
+        }
+
+        public function testFloatArrayWithNegativeLength()
+        {
+            $casus = new \solidew\Casus\MCrypt;
+            $val = $casus->floatArray(1,2,4,-100);
+            $this->assertInternalType('array', $val);
+            $this->assertEquals(count($val), 100);
+            $this->assertContainsOnly('float', $val);
+        }
+
+        public function testRandomizeWithString()
+        {
+            $casus = new \solidew\Casus\MCrypt;
+            $val = $casus->randomize('Potato');
+
+            $this->assertInternalType('string', $val);
+            $this->assertEquals(strlen($val), 6);
+            $this->assertTrue(in_array('P',str_split($val)));
+        }
+
+        public function testRandomizeWithArray()
+        {
+            $casus = new \solidew\Casus\MCrypt;
+            $val = $casus->randomize(['P','o','t','a','t','o']);
+
+            $this->assertInternalType('array', $val);
+            $this->assertEquals(count($val), 6);
+            $this->assertTrue(in_array('P', $val));
+        }
+
+        public function selectRandomWithString()
+        {
+            $casus = new \solidew\Casus\MCrypt;
+            $val = $casus->selectRandom('Potato', 3);
+
+            $this->assertInternalType('string', $val);
+            $this->assertEquals(strlen($val), 3);
+            $this->assertTrue(in_array(['P','o','t','a'],str_split($val)));
+        }
+
+        public function selectRandomWithArray()
+        {
+            $casus = new \solidew\Casus\MCrypt;
+            $val = $casus->selectRandom(['P','o','t','a','t','o'], 3);
+
+            $this->assertInternalType('array', $val);
+            $this->assertEquals(count($val), 3);
+            $this->assertTrue(in_array(['P','o','t','a'], $val));
+        }
+
+        public function selectRandomWithLengthBiggerThanInput()
+        {
+            $casus = new \solidew\Casus\MCrypt;
+            $val = $casus->selectRandom(['P','o','t','a','t','o'], 20);
+
+            $this->assertInternalType('array', $val);
+            $this->assertEquals(count($val), 6);
+            $this->assertTrue(in_array(['P','o','t','a'], $val));
+        }
+
+        public function selectRandomWithNegativeLength()
+        {
+            $casus = new \solidew\Casus\MCrypt;
+            $val = $casus->selectRandom(['P','o','t','a','t','o'], -20);
+
+            $this->assertInternalType('array', $val);
+            $this->assertEquals(count($val), 6);
+            $this->assertTrue(in_array(['P','o','t','a'], $val));
         }
     }
 }
